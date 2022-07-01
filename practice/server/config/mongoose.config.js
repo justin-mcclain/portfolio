bbe0bf792b1325de203377f2dacc5ce15358afba
justin-mcclain <justinamcclain@gmail.com>
2022-07-01 13:13:29 -0500

    updates

--- practice/server/config/mongoose.config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const dbname = "finances-db"
-
-mongoose.connect(`mongodb+srv://root:root@mern-march-db.fqptc.mongodb.net/${dbname}?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log('Established a connection to the database'))
-    .catch(err => console.log('Something went wrong when connecting to the database ', err));
